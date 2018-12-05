152e330e50e16e69ec51ed6169bbf01ba23476ed
jose ivan peñuelas <joseivanpenuelas@joses-MacBook-Pro.local>
2018-12-05 12:55:50 -0700

    first invite users screen

---
+++ app/src/main/java/com/rocketjourney/controlcenterrocketjourney/home/InviteUsersActivity.kt
@@ -0,0 +1,13 @@
+package com.rocketjourney.controlcenterrocketjourney.home
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.rocketjourney.controlcenterrocketjourney.R
+
+class InviteUsersActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_invite_users)
+    }
+}
