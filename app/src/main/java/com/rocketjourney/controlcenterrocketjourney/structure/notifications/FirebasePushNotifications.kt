d2efd9e5fb3be7e1419b676ecd5db61555ef5715
Jose Ivan <joseivanpo01@gmail.com>
2018-12-13 02:46:45 -0700

    merged push notifs and guide fragment, version to test to upload to playstore

--- app/src/main/java/com/rocketjourney/controlcenterrocketjourney/structure/notifications/FirebasePushNotifications.kt
+++ app/src/main/java/com/rocketjourney/controlcenterrocketjourney/structure/notifications/FirebasePushNotifications.kt
@@ -4,7 +4,9 @@ import android.content.Intent
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.google.gson.Gson
+import com.rocketjourney.controlcenterrocketjourney.LaunchActivity
 import com.rocketjourney.controlcenterrocketjourney.R
+import com.rocketjourney.controlcenterrocketjourney.structure.RJControlCenter
 import com.rocketjourney.controlcenterrocketjourney.structure.managers.NotificationManager
 import com.rocketjourney.controlcenterrocketjourney.structure.objects.PushNotificationsData
 
@@ -23,7 +25,7 @@ class FirebasePushNotifications : FirebaseMessagingService() {
         val title = NotificationManager.rjControlCenter
         var description = ""
 
-        var pendingIntent = Intent()
+        var pendingIntent = Intent(RJControlCenter.context, LaunchActivity::class.java)
 
         if (NotificationManager.NEW_CHECKIN_ON_SPOT_FOR_CC == typeNotif) {
             description = getString(R.string.user_checked_in_at, data.message.locArgs[0], data.message.locArgs[1])
