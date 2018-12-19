b88b16f51c7676a194a6a2cc6870e91eb85a730f
Jose Ivan <joseivanpo01@gmail.com>
2018-12-19 00:49:09 -0700

    commit

--- app/src/main/java/com/rocketjourney/controlcenterrocketjourney/structure/RJControlCenter.java
+++ app/src/main/java/com/rocketjourney/controlcenterrocketjourney/structure/RJControlCenter.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 
 import com.crashlytics.android.Crashlytics;
+
 import io.branch.referral.Branch;
 import io.fabric.sdk.android.Fabric;
 import io.realm.DynamicRealm;
